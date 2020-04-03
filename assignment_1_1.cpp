06a3deb391fb3fbbcf6accf8a26601e99b1c9c7b
magael <>
2020-04-03 23:51:34 +0300

    reading integers from binary working

--- assignment_1_1.cpp
+++ assignment_1_1.cpp
@@ -7,12 +7,12 @@ using namespace std;
 int main(int argc, char **argv)
 {
   if (argc < 2) throw logic_error("Please provide an input filename as a parameter.");
-  uint64_t i;
+  // uint64_t i;
   long n = 0;
   string filename = argv[1];
   ifstream infile(filename, ios::in | ios::binary);
   ofstream outfile(filename + ".vb", ios::out | ios::binary);
-  
+
   // // test
   // string test = "";
   // infile >> test;
@@ -26,17 +26,31 @@ int main(int argc, char **argv)
 
   streampos size;
   char * memblock;
+  uint64_t read_int;
 
   if (infile.is_open())
   {
-    size = infile.tellg();
-    memblock = new char[size];
+    // size = infile.tellg();
+    memblock = new char[64];
     infile.seekg(0, ios::beg);
-    infile.read(memblock, size);
-    outfile.write(memblock, size);
-    // infile.close();
+    // infile.read(memblock, 64);
 
-    cout << "the entire file content is in memory";
+    size = sizeof(uint64_t); // 8
+    infile.read(reinterpret_cast<char*>(&read_int), size);
+    cout << read_int << '\n';
+    outfile.write(reinterpret_cast<char*>(&read_int), size);
+
+    // ifstream infile(filename, ios::in | ios::binary);
+
+    // infile.seekg(sizeof(uint64_t));
+    // infile.read(reinterpret_cast<char*>(&read_int), size);
+    // cout << read_int << '\n';
+
+    // outfile.write(memblock, 64);
+    infile.close();
+
+    // cout << "the entire file content is in memory";
+    cout << "done";
 
     delete[] memblock;
   }
