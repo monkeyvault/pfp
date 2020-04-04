cb5ded5b1a17a37e6d172ee16ff54d504430a3f2
magael <>
2020-04-04 19:42:20 +0300

    vbyte encoding and writing bytes working

--- assignment_1_1.cpp
+++ assignment_1_1.cpp
@@ -22,47 +22,74 @@ int main(int argc, char **argv)
   //   outfile << i;
   //   n++;
   // }
+  // cout << n;
   // // end test
 
   streampos size;
-  char* memblock;
   uint64_t read_int;
 
   if (infile.is_open())
   {
-    // size = infile.tellg();
-    // memblock = new char[64];
-    infile.seekg(0, ios::beg);
-    // infile.read(memblock, 64);
-
+    // // check is output file empty
+    // cout << "tellg size" << infile.tellg() << '\n';
+    // if (infile.tellg() > 0) {
+    //   outfile.write("",0);
+    //   cout << "erased output file \n";
+    // }
+    // read first integer
     size = sizeof(uint64_t); // 8
+    infile.seekg(0, ios::beg);
     infile.read(reinterpret_cast<char*>(&read_int), size);
-    cout << read_int << '\n';
-    cout << "writing" << '\n';
-    outfile.write(reinterpret_cast<char*>(&read_int), size);
+    cout << "read int: " << read_int << '\n';
 
-    cout << "test read from .vb-file" << '\n';
-    ifstream infile_2(filename + ".vb", ios::in | ios::binary);
-    infile_2.seekg(0, ios::beg);
-    infile_2.read(reinterpret_cast<char*>(&read_int), size);
-    cout << read_int << '\n';
-    infile_2.close();
+    // vbyte encryption
+    // VByteEncodeInteger(i): (INPUT: one integer, OUTPUT: one or more bytes)
+    // while true do
+    //   b = i mod 128
+    //   if i < 128 then
+    //     OutputByte(b+128) and BREAK
+    //   OutputByte(b)
+    //   i ← i div 128
+    // end
+    uint64_t i = read_int;
+    int loops = 0;
+    while (loops < 10) {
+    // while (true) { // change to this from loop safeguard before submit
+      uint64_t b = i % 128;
+      if (i < 128) {
+        b += 128;
+        cout << b << '\n';
+        // write byte
+        outfile.write(reinterpret_cast<char*>(&b), size);
+        break;
+      }
+      cout << b << '\n';
+      outfile.write(reinterpret_cast<char*>(&b), size);
+      i /= 128;
+      loops++;
+    }
+    if (loops > 8) cout << "loops maxed \n";
 
+    // write integer
+    // outfile.write(reinterpret_cast<char*>(&read_int), size);
+    
+    // // read another integer
     // infile.seekg(sizeof(uint64_t));
     // infile.read(reinterpret_cast<char*>(&read_int), size);
     // cout << read_int << '\n';
 
-    // outfile.write(memblock, 64);
-    infile.close();
-
-    // cout << "the entire file content is in memory";
     cout << "done";
-
-    delete[] memblock;
   }
   else cout << "Unable to open file";
 
   infile.close();
   outfile.close();
-  // cout << n;
+
+  cout << "test read from .vb-file" << '\n';
+  uint64_t read_int_2;
+  ifstream infile_2(filename + ".vb", ios::in | ios::binary);
+  infile_2.seekg(8);
+  infile_2.read(reinterpret_cast<char*>(&read_int_2), size);
+  cout << read_int_2 << '\n';
+  infile_2.close();
 }
