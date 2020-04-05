7160d3f757290b9c08de62ee04f822899353e9cc
magael <>
2020-04-05 21:09:41 +0300

    encrypting all in a file

--- assignment_1_1.cpp
+++ assignment_1_1.cpp
@@ -1,86 +1,90 @@
-#include <iostream>
-#include <fstream>
 #include <stdint.h>
+#include <fstream>
+#include <iostream>
+
+#define size 8
 
 using namespace std;
 
-void encrypt_file(ifstream &infile, ofstream &outfile)
-{
-    streampos size;
-    uint64_t read_int;
-
-    // read first integer
-    size = sizeof(uint64_t); // 8
-    infile.seekg(0, ios::beg);
-    infile.read(reinterpret_cast<char *>(&read_int), size);
-    cout << "read int: " << read_int << '\n';
-
-    // vbyte encryption
+// vbyte encryption
+void encrypt_integer(uint64_t &read_int, ofstream &outf) {
     uint64_t i = read_int;
     // int loops = 0;
     // while (loops < 10) {
-    while (true)
-    {
+    while (true) {
         uint64_t b = i % 128;
-        if (i < 128)
-        {
+        if (i < 128) {
             b += 128;
             cout << b << '\n';
             // write byte
-            outfile.write(reinterpret_cast<char *>(&b), size);
+            outf.write(reinterpret_cast<char *>(&b), size);
             break;
         }
         cout << b << '\n';
-        outfile.write(reinterpret_cast<char *>(&b), size);
+        outf.write(reinterpret_cast<char *>(&b), size);
         i /= 128;
         // loops++;
     }
     // if (loops > 8) cout << "loops maxed \n";
+}
+
+void encrypt_file(ifstream &inf, ofstream &outf) {
+    uint64_t read_int;
+    long i{};
+    // read first integer
+    inf.seekg(0, ios::beg);
+    // while (inf) {
+    // DEBUG: swap "i < 100" to "inf" before submitting or testing on turso
+    while (i < 100) {
+        // inf.seekg(i * size);
+        inf.read(reinterpret_cast<char *>(&read_int), size);
+        cout << "read int: " << read_int << '\n';
+        encrypt_integer(read_int, outf);
+        i++;
+    }
+    cout << "read " << i << " integers";
 
     // // read another integer
-    // infile.seekg(sizeof(uint64_t));
-    // infile.read(reinterpret_cast<char*>(&read_int), size);
+    // inf.seekg(size);
+    // inf.read(reinterpret_cast<char*>(&read_int), size);
     // cout << read_int << '\n';
 
     cout << "done \n";
 }
 
-int main(int argc, char **argv)
-{
-    if (argc < 2)
-        throw logic_error("Please provide an input filename as a parameter.");
+int main(int argc, char **argv) {
+    if (argc < 2) throw logic_error("Please provide a filename");
     string filename = argv[1];
-    ifstream infile(filename, ios::in | ios::binary);
-    ofstream outfile(filename + ".vb", ios::out | ios::binary);
+    ifstream inf(filename, ios::in | ios::binary);
+    ofstream outf(filename + ".vb", ios::out | ios::binary);
 
     // // test
     // uint64_t i;
     // long n = 0;
     // string test = "";
-    // infile >> test;
+    // inf >> test;
     // cout << test << "\n";
     //
-    // while (infile >> i) {
-    //   outfile << i;
+    // while (inf >> i) {
+    //   outf << i;
     //   n++;
     // }
     // cout << n;
     // // end test
 
-    if (infile.is_open())
-    {
-        encrypt_file(infile, outfile);
-    }
-    else
+    if (inf.is_open() && outf.is_open()) {
+        encrypt_file(inf, outf);
+    } else {
         cout << "Unable to open file \n";
-    infile.close();
-    outfile.close();
+    }
+    inf.close();
+    outf.close();
 
     cout << "test read from .vb-file \n";
     uint64_t read_int_2;
-    ifstream infile_2(filename + ".vb", ios::in | ios::binary);
-    infile_2.seekg(8);
-    infile_2.read(reinterpret_cast<char *>(&read_int_2), 8);
+    ifstream inf_2(filename + ".vb", ios::in | ios::binary);
+    inf_2.seekg(size);
+    inf_2.read(reinterpret_cast<char *>(&read_int_2), size);
     cout << read_int_2 << '\n';
-    infile_2.close();
+    inf_2.close();
 }
