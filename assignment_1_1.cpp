9d3a6dcff8402d3b77e65645aca3bf71bda86415
magael <>
2020-04-06 00:20:26 +0300

    refactored, e.g. debugging to use ifdef

--- assignment_1_1.cpp
+++ assignment_1_1.cpp
@@ -1,48 +1,50 @@
-#include <stdint.h>
 #include <fstream>
 #include <iostream>
+#include <stdint.h>
+
+#define ENABLE_DEBUG // comment out to disable debugging
 
 using namespace std;
 
-// vbyte encryption
-void encrypt_integer(uint64_t i, ofstream &outf) {
+void output_byte(uint8_t &b, ofstream &outf) {
+    outf.write(reinterpret_cast<char *>(&b), sizeof(b));
+#ifdef ENABLE_DEBUG
+    cout << (int)b << '\n';
+#endif
+}
+
+// vbyte encoding
+void encode_integer(uint64_t i, ofstream &outf) {
     while (true) {
         uint8_t b = i % 128;
         if (i < 128) {
             b += 128;
-            cout << (int)b << '\n';
-            // write byte
-            outf.write(reinterpret_cast<char *>(&b), sizeof(b));
+            output_byte(b, outf);
             break;
         }
-        cout << (int)b << '\n';
-        outf.write(reinterpret_cast<char *>(&b), sizeof(b));
+        output_byte(b, outf);
         i /= 128;
     }
 }
 
-void encrypt_file(ifstream &inf, ofstream &outf) {
+void encode_file(ifstream &inf, ofstream &outf) {
     uint64_t i;
     long n{};
-    // read first integer
     inf.seekg(0, ios::beg);
-    // while (inf) {
-    // DEBUG: swap "i < 100" to "inf" before submitting or testing on turso
-    while (n < 100) {
-        // inf.seekg(i * size);
+
+    while (inf) {
         inf.read(reinterpret_cast<char *>(&i), sizeof(i));
-        cout << "read int: " << i << '\n';
-        encrypt_integer(i, outf);
+#ifdef ENABLE_DEBUG
+        if (n > 9) break;
         n++;
+        cout << "read int: " << i << '\n';
+#endif
+        encode_integer(i, outf);
     }
-    cout << "read " << n << " integers \n"; // F0: "read 1048577 integers"
 
-    // // read another integer
-    // inf.seekg(size);
-    // inf.read(reinterpret_cast<char*>(&i), sizeof(i));
-    // cout << i << '\n';
-
-    cout << "done \n";
+#ifdef ENABLE_DEBUG
+    cout << "encoded " << n << " integers \n"; // F0: 1048577 integers
+#endif
 }
 
 int main(int argc, char **argv) {
@@ -51,33 +53,21 @@ int main(int argc, char **argv) {
     ifstream inf(filename, ios::in | ios::binary);
     ofstream outf(filename + ".vb", ios::out | ios::binary);
 
-    // // test
-    // uint64_t i;
-    // long n = 0;
-    // string test = "";
-    // inf >> test;
-    // cout << test << "\n";
-    //
-    // while (inf >> i) {
-    //   outf << i;
-    //   n++;
-    // }
-    // cout << n;
-    // // end test
-
     if (inf.is_open() && outf.is_open()) {
-        encrypt_file(inf, outf);
+        encode_file(inf, outf);
     } else {
         cout << "Unable to open file \n";
     }
     inf.close();
     outf.close();
 
-    cout << "test read from .vb-file \n";
+#ifdef ENABLE_DEBUG
+    cout << "test read from .vb-file: \n";
     uint8_t i;
     ifstream inf_2(filename + ".vb", ios::in | ios::binary);
     inf_2.seekg(0, ios::beg);
     inf_2.read(reinterpret_cast<char *>(&i), sizeof(i));
     cout << (int)i << '\n';
     inf_2.close();
+#endif
 }
